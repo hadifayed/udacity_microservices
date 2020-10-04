220f288706d4b7619b0dcb2ebee7488fbbe859d1
hagfayed <hagfayed@gmail.com>
2020-10-04 02:18:44 +0200

    complete run_kubernetes.sh and save output from cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=hadifayed/udacity_microservices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run my-app --image=hadifayed/udacity_microservices:prediction_app --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward my-app 8000:80
